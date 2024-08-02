9e0f8888127cdec727295f268689e561a6a31991
Tomas Gentile <tomas.gentile@gm2dev.com>
2024-08-02 10:32:52 -0300

    feat(arrays): add arrays exercises

---
+++ arrays.js
@@ -0,0 +1,26 @@
+let numbers = [];
+numbers.push(1, 2, 3, 4, 5);
+console.log(numbers);
+numbers.pop();
+
+numbers.pop();
+console.log(numbers);
+numbers.unshift(4);
+
+let colors = [];
+colors.push("red", "blue", "green");
+colors.shift();
+
+let fruits = [];
+fruits.push("apple", "banana", "orange");
+fruits.pop();
+fruits.pop();
+
+fruits.unshift("grape", "kiwi");
+
+fruits.shift();
+fruits.shift();
+
+colors.push("yellow");
+
+colors.shift();
