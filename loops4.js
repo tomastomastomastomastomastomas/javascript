b3e0264a3f2f700f19df7875adfd2531fc3bbc57
Tomas Gentile <tomas.gentile@gm2dev.com>
2024-08-16 09:28:30 -0300

    feat(loops): add loop exercises

--- loops4.js
+++ loops4.js
@@ -1,3 +1,12 @@
+function invertirMensajes(mensaje) {
+  let mensajeNuevo = "";
+  for (let i = mensaje.length - 1; i >= 0; i--) {
+    mensajeNuevo += mensaje[i];
+  }
+  return mensajeNuevo;
+}
+let mensaje = "Che, ¿vamos a la cancha?";
+
 function calcularPromedio(calificacion) {
   let cont = 0;
   let prom = 0;
@@ -24,4 +33,23 @@ function calcularPromedio(calificacion) {
   }
   return estrellas;
 }
-calificacion = [4.5, 4.5];
+calificacion = [5, 5];
+
+function creatTabla(numero) {
+  let tabla = [];
+  for (let i = 1; i <= 10; i++) {
+    tabla.push(numero * i);
+  }
+  return tabla;
+}
+
+let puntaje = [3, 5, 7, 2, 8];
+function clashRoyal(puntaje) {
+  let maximo = puntaje[0];
+  for (let i = 0; i < puntaje.length; i++) {
+    if (maximo < puntaje[i + 1]) {
+      maximo = puntaje[i + 1];
+    }
+  }
+  return maximo;
+}
