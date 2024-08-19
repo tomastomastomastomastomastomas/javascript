7e0981aa35523ca9e6babc82be4657a4a88bf8d0
Tomas Gentile <tomas.gentile@gm2dev.com>
2024-08-19 12:31:44 -0300

    feat(dom): add dom exercises

--- dom.js
+++ dom.js
@@ -21,3 +21,20 @@ let header = document.querySelector(".header");
 if (header.classList.contains("sticky")) {
   console.log(header);
 }
+
+let miUl1 = document.querySelector("ul.create1");
+let miUl2 = document.querySelector("ul.create2");
+let datos1 = ["hola1", "hola2", "hola3"];
+let datos2 = ["chau1", "chau2", "chau3"];
+
+datos1.forEach(function (dato) {
+  let create = document.createElement("li");
+  create.textContent = dato;
+  miUl1.append(create);
+});
+
+datos2.forEach(function (dato) {
+  let create = document.createElement("li");
+  create.textContent = dato;
+  miUl2.append(create);
+});
